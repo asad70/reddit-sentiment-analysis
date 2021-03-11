3f6c83c0026043ff16d1665e607706a8c20a603a
Asad <asadidrees70@gmail.com>
2021-03-11 09:32:12 -0700

    handles large pay load error

--- reddit-sentiment-analysis.py
+++ reddit-sentiment-analysis.py
@@ -30,7 +30,7 @@ reddit = praw.Reddit(user_agent="Comment Extraction",
 
 '''############################################################################'''
 # set the program parameters
-subs = ['wallstreetbets', 'stocks', 'investing', 'stockmarket']     # sub-reddit to search
+subs = ['wallstreetbets' ]     # sub-reddit to search
 post_flairs = {'Daily Discussion', 'Weekend Discussion', 'Discussion'}    # posts flairs to search || None flair is automatically considered
 goodAuth = {'AutoModerator'}   # authors whom comments are allowed more than once
 uniqueCmt = True                # allow one comment per author per symbol
@@ -38,7 +38,7 @@ ignoreAuthP = {'example'}       # authors to ignore for posts
 ignoreAuthC = {'example'}       # authors to ignore for comment 
 upvoteRatio = 0.70         # upvote ratio for post to be considered, 0.70 = 70%
 ups = 20       # define # of upvotes, post is considered if upvotes exceed this #
-limit = 100      # define the limit, comments 'replace more' limit
+limit = 500     # define the limit, comments 'replace more' limit
 upvotes = 2     # define # of upvotes, comment is considered if upvotes exceed this #
 picks = 10     # define # of picks here, prints as "Top ## picks are:"
 picks_ayz = 5   # define # of picks for sentiment analysis
@@ -47,6 +47,7 @@ picks_ayz = 5   # define # of picks for sentiment analysis
 
 posts, count, c_analyzed, tickers, titles, a_comments = 0, 0, 0, {}, [], {}
 cmt_auth = {}
+
 
 for sub in subs:
     subreddit = reddit.subreddit(sub)
@@ -62,39 +63,42 @@ for sub in subs:
             comments = submission.comments
             titles.append(submission.title)
             posts += 1
-            submission.comments.replace_more(limit=limit)   
-            for comment in comments:
-                # try except for deleted account?
-                try: auth = comment.author.name
-                except: pass
-                c_analyzed += 1
-                
-                # checking: comment upvotes and author
-                if comment.score > upvotes and auth not in ignoreAuthC:      
-                    split = comment.body.split(" ")
-                    for word in split:
-                        word = word.replace("$", "")        
-                        # upper = ticker, length of ticker <= 5, excluded words,                     
-                        if word.isupper() and len(word) <= 5 and word not in blacklist and word in us:
-                            
-                            # unique comments, try/except for key errors
-                            if uniqueCmt and auth not in goodAuth:
-                                try: 
-                                    if auth in cmt_auth[word]: break
-                                except: pass
+            try: 
+                submission.comments.replace_more(limit=limit)   
+                for comment in comments:
+                    # try except for deleted account?
+                    try: auth = comment.author.name
+                    except: pass
+                    c_analyzed += 1
+                    
+                    # checking: comment upvotes and author
+                    if comment.score > upvotes and auth not in ignoreAuthC:      
+                        split = comment.body.split(" ")
+                        for word in split:
+                            word = word.replace("$", "")        
+                            # upper = ticker, length of ticker <= 5, excluded words,                     
+                            if word.isupper() and len(word) <= 5 and word not in blacklist and word in us:
                                 
-                            # counting tickers
-                            if word in tickers:
-                                tickers[word] += 1
-                                a_comments[word].append(comment.body)
-                                cmt_auth[word].append(auth)
-                                count += 1
-                            else:                               
-                                tickers[word] = 1
-                                cmt_auth[word] = [auth]
-                                a_comments[word] = [comment.body]
-                                count += 1    
-
+                                # unique comments, try/except for key errors
+                                if uniqueCmt and auth not in goodAuth:
+                                    try: 
+                                        if auth in cmt_auth[word]: break
+                                    except: pass
+                                    
+                                # counting tickers
+                                if word in tickers:
+                                    tickers[word] += 1
+                                    a_comments[word].append(comment.body)
+                                    cmt_auth[word].append(auth)
+                                    count += 1
+                                else:                               
+                                    tickers[word] = 1
+                                    cmt_auth[word] = [auth]
+                                    a_comments[word] = [comment.body]
+                                    count += 1   
+            except Exception as e: print(e)
+            
+                       
 
 # sorts the dictionary
 symbols = dict(sorted(tickers.items(), key=lambda item: item[1], reverse = True))
@@ -124,6 +128,8 @@ vader = SentimentIntensityAnalyzer()
 vader.lexicon.update(new_words)
 
 picks_sentiment = list(symbols.keys())[0:picks_ayz]
+
+
 for symbol in picks_sentiment:
     stock_comments = a_comments[symbol]
     for cmnt in stock_comments:
@@ -155,10 +161,10 @@ print(df)
 squarify.plot(sizes=times, label=top, alpha=.7 )
 plt.axis('off')
 plt.title(f"{picks} most mentioned picks")
-#plt.show()
+plt.show()
 
 # Sentiment analysis
 df = df.astype(float)
 colors = ['red', 'springgreen', 'forestgreen', 'coral']
 df.plot(kind = 'bar', color=colors, title=f"Sentiment analysis of top {picks_ayz} picks:")
-#plt.show()
+plt.show()
